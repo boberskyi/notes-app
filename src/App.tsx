f2f79e8ef781d10e9fa1494614682f41db0b21bf
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-12-16 10:34:09 +0100

    Create basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import {Navigate, Route, Routes} from 'react-router-dom'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Container from 'react-bootstrap/esm/Container'
 
 export const App = () => {
-  return <h1>App</h1>
+    return (
+        <Container className={'my-4'}>
+            <Routes>
+                <Route path={'/'} element={<h1>Home</h1>}/>
+                <Route path={'/new'} element={<h1>New</h1>}/>
+                <Route path={'/:id'}>
+                    <Route index element={<h1>Show</h1>}/>
+                    <Route path={'edit'} element={<h1>Edit</h1>}/>
+                </Route>
+                <Route path={'*'} element={<Navigate to={'/'}/>}/>
+            </Routes>
+        </Container>
+    )
 }
 
 export default App
